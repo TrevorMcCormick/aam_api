013c7c1a540a8cd76f013065c1eff9e4d58ff305
Trevor McCormick <trevor.ryan.mccormick@gmail.com>
2020-02-19 09:30:42 -0500

    v42

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["pandas>=0.24", "xlrd>=1", "requests>=2"]
 
 setup(
     name="aam_api",
-    version="0.0.40",
+    version="0.0.42",
     author="Trevor McCormick",
     author_email="trevor.ryan.mccormick@gmail.com",
     description="Adobe Audience Manager API Python Wrapper",
